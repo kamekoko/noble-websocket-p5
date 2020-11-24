523ec890580c693e1c5ffe370879c5714273cc29
kamekoko <68806725+kamekoko@users.noreply.github.com>
2020-11-24 18:45:55 +0900

    minor change

--- noble-websocket-p5-sample/server.js
+++ noble-websocket-p5-sample/server.js
@@ -11,25 +11,7 @@ app.use(express.static('public'));
 var socket = require('socket.io');
 var io = socket(server);
 
-
-// noble
-const os = require('os');
-const { json } = require('express');
-if (os.platform() === 'win32') {
-    const ver = os.release().split('.').map(Number);
-    if (!(ver[0] > 10 ||
-        (ver[0] === 10 && ver[1] > 0) ||
-        (ver[0] === 10 && ver[1] === 0 && ver[2] >= 15014))) {
-        throw new Error('Noble WinRT bindings require Windows >= 10.0.15014.');
-    }
-
-    const Noble = require('noble/lib/noble');
-    const { WinrtBindings } = require('./bindings.js');
-    module.exports = new Noble(new WinrtBindings());
-} else {
-    module.exports = require('noble');
-}
-const noble = module.exports;
+const noble = require('noble-winrt');
 
 
 // log file
