43406c8ea59dcbe26f5427c9b4962d37cb53b650
kamekoko <68806725+kamekoko@users.noreply.github.com>
2021-03-05 21:26:34 +0900

    minor change

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var num = 0;
+var numCopy = num;
+
+console.log(num);
+console.log(numCopy);
+
+numCopy++;
+
+console.log(num);
+console.log(numCopy);
--- test/trilateration.js
+++ test/trilateration.js
@@ -1,27 +1,10 @@
 var trilat = require('trilat');
-
-class Node { // beacon for trilateration (three only)
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.dist = 0;
-    }
-    updateDist(dist) {
-        this.dist = dist;
-    }
-    updatePos(newX, newY) {
-        this.x = newX;
-        this.y = newY;
-    }
-}
-
-var node1 = new Node(0, 0);
-var node2 = new Node(5, 0);
-var node3 = new Node(5, 5);
+require('date-utils');
+var now = new Date();
 
 let createRssi = () => {
     var min = -80;
-    var max = -50;
+    var max = -60;
     let rssi = Math.floor(Math.random() * (max + 1 - min)) + min;
     return  rssi;
 }
@@ -32,21 +15,79 @@ let rssiToDist = (rssi) => {
     return dist;
 }
 
-var getTrilatInput = (node1, node2, node3) => {
-    console.log(node1);
-    console.log(node2);
-    console.log(node3);
+class Beacon { // beacon for trilateration (three only)
+    constructor(_index, _x, _y) {
+        this.index = _index;
+        this.x = _x;
+        this.y = _y;
+        this.rssi = 0;
+        this.dist = 0;
+        now = new Date();
+        this.time = now.toFormat('HH24:MI:SS');
+    }
+    updateRssi(_rssi) {
+        this.rssi = _rssi
+        this.dist = rssiToDist(_rssi);
+        this.time = now.toFormat('HH24:MI:SS');
+    }
+    updatePos(newX, newY) {
+        this.x = newX;
+        this.y = newY;
+    }
+}
+
+
+var fingerprint = [];
+
+var getTrilatInput = (_index1, _index2, _index3) => {
     return  [
-        [node1.x, node1.y, node1.dist],
-        [node2.x, node2.y, node2.dist],
-        [node3.x, node3.y, node3.dist]
+        [fingerprint[_index1].x, fingerprint[_index1].y, fingerprint[_index1].dist],
+        [fingerprint[_index2].x, fingerprint[_index2].y, fingerprint[_index2].dist],
+        [fingerprint[_index3].x, fingerprint[_index3].y, fingerprint[_index3].dist],
     ];
 }
 
-node1.addDist(rssiToDist(createRssi()));
-node2.addDist(rssiToDist(createRssi()));
-node3.addDist(rssiToDist(createRssi()));
+let getTopThreeRssi = () => {
+    var ans = [];
+    var num = 0;
 
-var output = trilat(getTrilatInput(node1, node2, node3));
+    var fpCopy = [];
+    for (let i = 0; i < fingerprint.length; i++) {
+        fpCopy[i] = fingerprint[i].rssi;
+    }
+
+    while (num < 3) {
+        var topIndex = 0;
+        var topVal = fpCopy[0]; 
+        for (let i = 1; i < fpCopy.length; i++) {
+            if (topVal < fpCopy[i]) {
+                topIndex = i;
+                topVal = fpCopy[i];
+            }
+        }
+        ans[num] = topIndex;
+        fpCopy[topIndex] = -1000;
+        num++;
+    }
+
+    return ans;
+}
+
+fingerprint[0] = new Beacon(0, 0, 0);
+fingerprint[1] = new Beacon(1, 5, 0);
+fingerprint[2] = new Beacon(2, 5, 5);
+fingerprint[3] = new Beacon(3, 0, 5);
+fingerprint[4] = new Beacon(4, 10, 0);
+for (let i = 0; i < fingerprint.length; i++) {
+    fingerprint[i].updateRssi(createRssi());
+}
+
+for (let i = 0; i < fingerprint.length; i++) {
+    console.log(fingerprint[i].index + " : " + fingerprint[i].rssi);
+}
+var input = getTopThreeRssi();
+console.log(input);
+
+var output = trilat(getTrilatInput(input[0], input[1], input[2]));
 
 console.log(output);
